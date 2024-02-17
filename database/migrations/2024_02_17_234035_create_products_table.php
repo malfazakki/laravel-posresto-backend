8aec5c42a8d1008d02eb9d6f094ef4a2a364ec8c
Al Fatih Muzakki <malfazakki@gmail.com>
2024-02-17 23:59:43 +0700

    create model, migration, factory, and seed for Product

--- database/migrations/2024_02_17_234035_create_products_table.php
+++ database/migrations/2024_02_17_234035_create_products_table.php
@@ -4,15 +4,21 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
+    public function up(): void {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('category_id')->constrained('categories');
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->string('image')->nullable();
+            $table->decimal('price', 10, 2);
+            $table->integer('stock');
+            $table->boolean('status')->default(1);
+            $table->boolean('is_favorite')->default(0);
             $table->timestamps();
         });
     }
@@ -20,8 +26,7 @@ return new class extends Migration
     /**
      * Reverse the migrations.
      */
-    public function down(): void
-    {
+    public function down(): void {
         Schema::dropIfExists('products');
     }
 };
